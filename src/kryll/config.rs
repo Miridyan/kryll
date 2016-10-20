e7d73e74f5f7dbba713ce80c5aee7c5d794ecd83
Miridyan <Michael.R.Ditto@gmail.com>
2016-10-19 23:02:10 -0400

    changed some more config stuff, removed unnecessary `use` statements, etc

--- src/kryll/config.rs
+++ src/kryll/config.rs
@@ -10,14 +10,14 @@ pub struct Config {
 }
 
 #[derive(Debug, RustcDecodable)]
-pub struct SiteConfig {
+struct SiteConfig {
     name: Option<String>,
     template: Option<String>,
     author: Option<Vec<String>>, 
 }
 
 #[derive(Debug, RustcDecodable)]
-pub struct DirConfig {
+struct DirConfig {
     out: Option<String>,
     source: Option<String>,
     images: Option<String>,
@@ -27,7 +27,7 @@ impl Config {
     pub fn new() -> Config {
         let config_file = File::open("kryll.toml");
         let config_str = handle_file_string(config_file, "kryll.toml");
-
+        
         let config_decode: Config = decode_str(&config_str).unwrap();
         config_decode
     }
